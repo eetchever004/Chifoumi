b974fd7e487f36f33ecafc0eb100b5e91fea004a
eetchever004 <99280669+eetchever004@users.noreply.github.com>
2022-05-30 10:51:34 +0200

    v4 sans doc
    v4 fonctionnelle, documentation à peaufiner avant rendu.

---
+++ v4/chifoumipresentation.cpp
@@ -0,0 +1,190 @@
+#include "chifoumipresentation.h"
+#include "chifoumivue.h"
+#include <QDebug>
+#include <QMessageBox>
+
+ChifoumiPresentation::ChifoumiPresentation(QObject *parent)
+    : QObject{parent}
+{
+    etatPartie = initial;
+    //ctor
+
+}
+
+Chifoumi *ChifoumiPresentation::getModele()
+{
+    return chifoumiModele;
+}
+
+ChifoumiVue *ChifoumiPresentation::getVue()
+{
+    return chifoumiVue;
+}
+
+void ChifoumiPresentation::setModele(Chifoumi *m)
+{
+    chifoumiModele = m;
+}
+
+void ChifoumiPresentation::setVue(ChifoumiVue *v)
+{
+    chifoumiVue = v;
+}
+
+ChifoumiPresentation::UnEtat ChifoumiPresentation::getEtat()
+{
+    return etatPartie;
+}
+
+void ChifoumiPresentation::setEtat(UnEtat e)
+{
+    etatPartie = e;
+}
+
+void ChifoumiPresentation::finDePartie(unsigned int limite)
+{
+    switch(etatPartie)
+    {
+    case initial:
+    {
+        break;
+    }
+    case enCours:
+    {
+        QMessageBox *fenetreFin = new QMessageBox;
+        if (getModele()->getScoreJoueur()==limite)
+        {
+            QString strLimite;
+            strLimite.setNum(limite);
+            fenetreFin->setText(" Bravo Joueur ! Vous gagnez avec " + strLimite + " points.");
+            fenetreFin->show();
+            setEtat(initial);
+            getVue()->griserElements();
+        }
+
+        else if (getModele()->getScoreMachine()==limite)
+        {
+            QString strLimite;
+            strLimite.setNum(limite);
+            fenetreFin->setText(" Bravo Machine ! Vous gagnez avec " + strLimite + " points.");
+            fenetreFin->show();
+            setEtat(initial);
+            getVue()->griserElements();
+        }
+        break;
+    }
+    }
+}
+
+void ChifoumiPresentation::nouvellePartie()
+{
+    switch(this->getEtat())
+    {
+        case initial:
+            getModele()->initCoups();
+            getModele()->initScores();
+            getVue()->majElementsInteractifs(this->getEtat());
+            getVue()->afficherValeursJeu(getModele()->getScoreJoueur(),
+                                         getModele()->getScoreMachine(),
+                                         getModele()->getCoupJoueur(),
+                                         getModele()->getCoupMachine(),
+                                         getModele()->getLimite());
+            setEtat(enCours);
+            break;
+        case enCours:
+            getModele()->initCoups();
+            getModele()->initScores();
+            getVue()->afficherValeursJeu(getModele()->getScoreJoueur(),
+                                         getModele()->getScoreMachine(),
+                                         getModele()->getCoupJoueur(),
+                                         getModele()->getCoupMachine(),
+                                         getModele()->getLimite());
+            break;
+    }
+}
+
+void ChifoumiPresentation::choixPierre()
+{
+    switch(etatPartie)
+    {
+        case initial:
+            break;
+        case enCours:
+            // pas de changement d'état
+        // activité 1
+            // maj modele
+            getModele()->setCoupJoueur(Chifoumi::pierre);
+            getModele()->setCoupMachine(getModele()->genererUnCoup());
+            getModele()->majScores(getModele()->determinerGagnant());
+
+            // ordonne à la vue de se mettre à jour en fonction du modele
+            // et de l'état du jeu
+            getVue()->afficherValeursJeu(getModele()->getScoreJoueur(),
+                                         getModele()->getScoreMachine(),
+                                         getModele()->getCoupJoueur(),
+                                         getModele()->getCoupMachine(),
+                                         getModele()->getLimite());
+            getVue()->majElementsInteractifs(this->getEtat());
+            finDePartie(getModele()->getLimite());
+        break;
+        default:break;
+    }
+}
+
+void ChifoumiPresentation::choixPapier()
+{
+    switch(etatPartie)
+    {
+        case initial:
+            break;
+        case enCours:
+            // pas de changement d'état
+        // activité 1
+            // maj modele
+            getModele()->setCoupJoueur(Chifoumi::papier);
+            getModele()->setCoupMachine(getModele()->genererUnCoup());
+            getModele()->majScores(getModele()->determinerGagnant());
+
+            // ordonne à la vue de se mettre à jour en fonction du modele
+            // et de l'état du jeu
+            getVue()->afficherValeursJeu(getModele()->getScoreJoueur(),
+                                         getModele()->getScoreMachine(),
+                                         getModele()->getCoupJoueur(),
+                                         getModele()->getCoupMachine(),
+                                         getModele()->getLimite());
+            getVue()->majElementsInteractifs(this->getEtat());
+            finDePartie(getModele()->getLimite());
+        break;
+        default:break;
+    }
+}
+
+void ChifoumiPresentation::choixCiseaux()
+{
+    switch(etatPartie)
+    {
+        case initial:
+            break;
+        case enCours:
+            // pas de changement d'état
+        // activité 1
+            // maj modele
+            getModele()->setCoupJoueur(Chifoumi::ciseau);
+            getModele()->setCoupMachine(getModele()->genererUnCoup());
+            getModele()->majScores(getModele()->determinerGagnant());
+
+            // ordonne à la vue de se mettre à jour en fonction du modele
+            // et de l'état du jeu
+            getVue()->afficherValeursJeu(getModele()->getScoreJoueur(),
+                                         getModele()->getScoreMachine(),
+                                         getModele()->getCoupJoueur(),
+                                         getModele()->getCoupMachine(),
+                                         getModele()->getLimite());
+            getVue()->majElementsInteractifs(this->getEtat());
+            finDePartie(getModele()->getLimite());
+        break;
+        default:break;
+    }
+}
+
+
---
+++ v4/chifoumivue.h
@@ -0,0 +1,55 @@
+#ifndef CHIFOUMIVUE_H
+#define CHIFOUMIVUE_H
+
+#include <QMainWindow>
+#include "chifoumipresentation.h"
+#include "chifoumi.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ChifoumiVue; }
+QT_END_NAMESPACE
+
+class ChifoumiPresentation;
+class ChifoumiVue : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ChifoumiVue(QWidget *parent = nullptr);
+    ~ChifoumiVue();
+
+    void setPresentation (ChifoumiPresentation * p);
+    ChifoumiPresentation* getPresentation();
+
+    // Ordres que la Présentation donne à la vue
+    void afficherValeursJeu(unsigned int sJ,
+                           unsigned int sM,
+                           Chifoumi::UnCoup cJ,
+                           Chifoumi::UnCoup cM,
+                           unsigned int limite);
+
+    void majElementsInteractifs(ChifoumiPresentation::UnEtat e);
+
+    void griserElements();
+
+    // slots qui réagissent aux événements de l'utilisateur
+    // et qui relaient cete info à la Présentation
+private slots:
+    void nouvellePartie();
+    void choixPierre();
+    void choixCiseaux();
+    void choixPapier();
+    // Ouvre le MsgBox du menu à propos
+    void ouvrirAPropos();
+
+private:
+    Ui::ChifoumiVue *ui;
+    ChifoumiPresentation* chifoumiPresentation;
+
+private:
+    // retourene le chemin de l'image représentant la figure c
+    QString nomImageDuCpoup (Chifoumi::UnCoup c);
+
+
+};
+#endif // CHIFOUMIVUE_H
